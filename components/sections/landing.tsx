c71e67df0dcde71fa93c8d0a0426840372207dab
Kim Björkman <hello@bjorkman.kim>
2025-03-01 22:17:56 +0900

    feat: introductary text to landing page

--- components/sections/landing.tsx
+++ components/sections/landing.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Text from "@/components/ui/text";
 import Container from "@/components/ui/container";
+import Link from "../ui/link";
 
 export function Landing({ ...props }: React.ComponentProps<"div">) {
   return (
@@ -9,13 +10,19 @@ export function Landing({ ...props }: React.ComponentProps<"div">) {
       {...props}
     >
       <div className="mx-auto flex flex-col gap-8 sm:gap-16 items-center justify-center">
-        <div className="px-8 sm:px-20 mr-auto">
+        <div className="px-8 sm:px-20 mr-auto flex flex-col items-center justify-items-center">
           <Text variant="display">AKIZUKI</Text>
+          <Text variant="lead" color="muted">
+            A hidden gem in Fukuoka, Japan |{" "}
+            <Link underline href="https://maps.app.goo.gl/BEsHf75AnMdM8Tpp6">
+              33°27 N, 130°41 E
+            </Link>
+          </Text>
         </div>
       </div>
       <Image
         src="/images/drawing.jpg"
-        alt="old castle drawing"
+        alt="An old castle drawing"
         width={1648}
         height={1540}
         className="h-full object-cover absolute -z-20 mix-blend-multiply dark:mix-blend-screen opacity-10 dark:invert"
