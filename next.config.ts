887ca668300a5f41bc92692715fc7e9a38ecd609
Kim Björkman <hello@bjorkman.kim>
2025-03-05 21:46:01 +0900

    feat: revert image loader

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    loader: "custom",
-    loaderFile: "lib/image-loader.js",
-  },
 };
 
 export default withPayload(nextConfig);
