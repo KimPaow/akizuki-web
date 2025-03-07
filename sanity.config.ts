c29457c3c75e26bd2f3af7db3960822c8ea91545
Kim Björkman <hello@bjorkman.kim>
2025-03-07 15:56:39 +0900

    fix: maybe?

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,21 @@
-import { Config, defineConfig } from "sanity";
+import { defineConfig, PluginOptions, SingleWorkspace } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schema } from "./sanity/schemas";
 import { jaJPLocale } from "@sanity/locale-ja-jp";
 import { esESLocale } from "@sanity/locale-es-es";
 import { apiVersion, dataset, projectId } from "./sanity/env";
+
+const plugins: PluginOptions[] = [
+  structureTool(),
+  // Vision is a tool that lets you query your content with GROQ in the studio
+  // https://www.sanity.io/docs/the-vision-plugin
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore This is complaining that "Types of property 'schema' are incompatible."
+  visionTool({ defaultApiVersion: apiVersion }),
+  esESLocale(),
+  jaJPLocale(),
+];
 
 const config = {
   name: "default",
@@ -14,17 +25,8 @@ const config = {
   // This is fine since we're more concerned about flexibility than security in this case
   projectId,
   dataset,
-  plugins: [
-    structureTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore This is complaining that "Types of property 'schema' are incompatible."
-    visionTool({ defaultApiVersion: apiVersion }),
-    esESLocale(),
-    jaJPLocale(),
-  ],
+  plugins,
   schema,
-} satisfies Config;
+} satisfies SingleWorkspace;
 
 export default defineConfig(config);
