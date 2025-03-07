b73a751fc5663469af747115dbe1380d1eded5e6
Kim Björkman <hello@bjorkman.kim>
2025-03-07 15:11:10 +0900

    fix: maybe fixed it?

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,11 @@
-import { Config, defineConfig } from "sanity";
+import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./sanity/schemaTypes";
 import { jaJPLocale } from "@sanity/locale-ja-jp";
 import { esESLocale } from "@sanity/locale-es-es";
-// import {defineDocuments, presentationTool} from 'sanity/presentation'
 
-const config: Config = {
+const config = {
   name: "default",
   title: "akizuki",
   basePath: "/admin",
@@ -15,29 +14,7 @@ const config: Config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "60vbi69u",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
 
-  plugins: [
-    structureTool(),
-    visionTool(),
-    esESLocale(),
-    jaJPLocale(),
-    // presentationTool({
-    //   resolve: {
-    //     mainDocuments: defineDocuments([
-    //       {
-    //         route: '/:slug',
-    //         filter: `_type == "page" && slug.current == $slug`,
-    //       },
-    //     ]),
-    //   },
-    //   previewUrl: {
-    //     origin: 'http://localhost:3000',
-    //     previewMode: {
-    //       enable: '/api/draft-mode/enable',
-    //       disable: '/api/draft-mode/disable',
-    //     },
-    //   },
-    // }),
-  ],
+  plugins: [structureTool(), visionTool(), esESLocale(), jaJPLocale()],
 
   schema: {
     types: schemaTypes,
