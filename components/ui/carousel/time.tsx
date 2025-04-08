db2a9f5526e70a8db5d1841ea5a73a0253d04d21
Kim Björkman <hello@bjorkman.kim>
2025-04-08 21:00:40 +0900

    fix: slideshow timing

--- components/ui/carousel/time.tsx
+++ components/ui/carousel/time.tsx
@@ -16,31 +16,27 @@ export const Slideshow = ({ slides }: { slides: Slide[] }) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const size = useMemo(() => slides.length, [slides.length]);
   const timer = useRef<NodeJS.Timeout | number>(-1);
-  console.log("slides:", slides);
 
   // Set the active image based on timer
   useEffect(() => {
-    timer.current = setInterval(
-      () => setActiveIndex((cur) => (cur + 1) % size),
-      8000
-    );
+    timer.current = setInterval(() => {
+      setActiveIndex((cur) => (cur + 1) % size);
+    }, 5000);
 
     return () => clearInterval(timer.current as number);
   }, [size]);
 
   return (
-    <div>
+    <div className="absolute w-full h-full">
       {slides.map((slide, i) => {
-        // const isPreviousActiveIndex = (activeIndex + size - 1) % size === i;
-        const isActiveIndex = activeIndex === i;
-
         return (
           <MotionImage
             variants={{
               hidden: { opacity: 0, transition: { duration: 3 } },
               visible: { opacity: 1, transition: { duration: 3 } },
             }}
-            animate={isActiveIndex ? "visible" : "hidden"}
+            initial="hidden"
+            animate={activeIndex === i ? "visible" : "hidden"}
             key={slide.id}
             src={slide.src}
             alt={slide.alt}
